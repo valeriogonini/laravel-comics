fab39035c25ddf18a2659256ac9f49c9a136b1b3
valeriogonini <valeriogonini@gmail.com>
2024-05-25 11:04:07 +0200

    add comic

--- resources/views/comics.blade.php
+++ resources/views/comics.blade.php
@@ -1,6 +1,22 @@
 @extends('layouts.app')
 @section('content')
-<div>
-    comics
+<div class="container">
+    <div class="row gy-3">
+        @foreach($comics as $comic)
+        <div class="col-4">
+            <div class="card h-100" style="width: 18rem;">
+                <img src="{{$comic['thumb']}}" class="card-img-top" alt="...">
+                <div class="card-body">
+                    <h5 class="card-title">{{$comic['title']}}</h5>
+                    <p class="card-text">{{$comic['series']}}</p>
+                    <p class="card-text">{{$comic['description']}}</p>
+                </div>
+            </div>
+        </div>
+        @endforeach
+
+    </div>
+
 </div>
+
 @endsection
